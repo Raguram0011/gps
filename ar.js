226376b9bf17fbbe0e6ae140bd030c8bbc90dc79
Raguram0011 <154828587+Raguram0011@users.noreply.github.com>
2025-09-14 14:14:48 +0530

    Update ar.js

--- ar.js
+++ ar.js
@@ -1,13 +1,14 @@
-function addARMarkers(type, places){
-  let arContainer=document.getElementById("arMarkers");
-  arContainer.innerHTML="";
-  places.forEach(el=>{
-    let lat=el.lat || (el.center && el.center.lat);
-    let lon=el.lon || (el.center && el.center.lon);
-    let name=el.tags.name || type;
-    let color=type==="hospital"?"red":"blue";
+function addARMarkers(type, places) {
+  let arContainer = document.getElementById("arMarkers");
+  arContainer.innerHTML = "";
 
-    let marker=document.createElement("a-text");
+  places.forEach(el => {
+    let lat = el.lat || (el.center && el.center.lat);
+    let lon = el.lon || (el.center && el.center.lon);
+    let name = el.tags.name || type;
+    let color = type === "hospital" ? "red" : "blue";
+
+    let marker = document.createElement("a-text");
     marker.setAttribute("value", name);
     marker.setAttribute("look-at", "[gps-camera]");
     marker.setAttribute("gps-entity-place", `latitude: ${lat}; longitude: ${lon};`);
